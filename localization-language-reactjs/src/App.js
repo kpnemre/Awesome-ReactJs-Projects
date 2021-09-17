29548ffc9543a3984be570391d6ed0740b46ca21
kpnemre <kpnemre51@gmail.com>
2021-09-17 17:50:17 +0300

    Project Finished

--- localization-language-reactjs/src/App.js
+++ localization-language-reactjs/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
 import './App.css';
+import {useState, useEffect} from "react"
+
+
+const message = {
+  'tr-TR':{
+title:"Merhaba Dünya",
+description:"Çoklu dil seçeneği mevcuttur"
+  },
+  'en-US':{
+    title:"Hello world",
+description:"There is multilangıage option"
+
+  }
+}
 
 function App() {
+const isLocal=localStorage.getItem("local");
+const defaultlocal=isLocal? isLocal:navigator.language;
+
+console.log(defaultlocal);
+const [local, setLocal] = useState(defaultlocal)
+
+
+
+useEffect(() => {
+localStorage.setItem("local",local)
+}, [local])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <IntlProvider messages={message[local]}locale="tr" defaultLocale="en">
+      <p className="text">
+      <FormattedMessage className="text" id="title"/>
+
+      </p>
+
+      <br />
+      <p className="text">
+      <FormattedMessage  id="description"/>
+
+      </p>
+        
+
+      </IntlProvider>
+      <button className="btn btn-1" onClick={()=>setLocal("tr-TR")}>Türkçe</button>
+<button className="btn btn-2" onClick={()=>setLocal("en-US")}>English</button>
     </div>
   );
 }
