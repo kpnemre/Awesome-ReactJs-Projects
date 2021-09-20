3a2afc48adcd65cc4ea54ec725dedf2d5cb72949
kpnemre <kpnemre51@gmail.com>
2021-09-20 22:39:35 +0300

    Project Finished

--- todoapp_contextapi_review/src/App.js
+++ todoapp_contextapi_review/src/App.js
@@ -1,9 +1,68 @@
+import { useState, useContext } from "react";
+import "./App.css";
+import { TodoContext } from "./context/TodoContext";
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const {todos, setTodos}= useContext(TodoContext)
+  console.log("data:", todos);
+
+  const clickButton = () => {
+    input
+      ? setTodos([
+          ...todos,
+          { id: todos.length + 1, title: input, done: false }
+        ])
+      : alert("Input giriniz");
+    setInput("");
+  };
   return (
+    
     <div className="App">
-      <h1>hello world</h1>
-      
+      <div className="head">
+        <h1>Yapılacaklar Listesi</h1>
+      </div>
+
+      <div className="todo-input">
+        <input
+          className="input"
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button className="btn" onClick={() => clickButton()}>
+          Ekle
+        </button>
+      </div>
+      <div className="todos">
+        {todos
+          ? todos.map((todo) => (
+              <div
+                onClick={() =>
+                  setTodos(
+                    todos.map((item) =>
+                      item.id === todo.id ? { ...item, done: !item.done } : item
+                    )
+                  )
+                }
+                key={todo.id}
+                className={todo.done ? "todo completed" : "todo"}
+              >
+                <p>{todo.title}</p>
+                {/* <p>{JSON.stringify(todo.done)}</p> */}
+              </div>
+            ))
+          : "Planlı Faaliyet Yok"}
+      </div>
+      <button
+        className="btn"
+        onClick={() => setTodos(todos.filter((item) => !item.done))}
+      >
+        Tamamlananları Temizle
+      </button>
     </div>
   );
 }
---
+++ todoapp_contextapi_review/src/context/TodoContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState, useEffect } from "react";
+
+export const TodoContext =createContext();
+
+const INITIAL_STATE = [];
+
+export const TodoProvider=({children})=>{
+    const [todos, setTodos] = useState(INITIAL_STATE)
+    const values={
+        todos,setTodos
+    }
+return(
+    <TodoContext.Provider value={values}>
+        {children}
+    </TodoContext.Provider>
+)
+}
